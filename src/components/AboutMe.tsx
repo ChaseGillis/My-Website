91121cfcf74c810d670225dc9c2a037ebb2f8265
Chase Gillis <125314353+ChaseGillis@users.noreply.github.com>
2024-06-09 19:45:48 -0400

    Update AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,9 +8,11 @@ const AboutMe: React.FC = () => {
       </div>
       <div className="about-text">
         <h2>ABOUT</h2>
-        <p>As a senior at New York University pursuing a Bachelor of Arts in Computer Science with a minor in Data Science, I have honed my skills in software engineering and data analysis. My academic journey and real-world experiences have fueled my passion for building fun projects that drive business success and technological advancement.
+        <p>Hello, and welcome to my personal website built with React, TypeScript, HTML, and CSS!
         <br/><br/>
-        I combine my expertise in Python, SQL, Java, and JavaScript with the work ethic and resilience I cultivated through being a student-athlete to tackle real-world problems and create impactful solutions. All of my previous roles, from SWE Consulting to leading a project dedicated to aiding legal research, involved optimizing systems or improving processes, aligning with my ultimate goal of using technology to make an impact.
+        As a senior at New York University pursuing a Bachelor of Arts in Computer Science with a minor in Data Science, I have honed my skills in software engineering and data analysis. My academic journey and real-world experiences have fueled my passion for building fun projects that drive business success and technological advancement.
+        <br/><br/>
+        I combine my expertise in Python, SQL, Java, and JavaScript with the work ethic and resilience I cultivated through being a student-athlete and state champion wrestler to tackle real-world problems and create impactful solutions. All of my previous roles, from SWE Consulting to leading a project dedicated to aiding legal research, involved optimizing systems or improving processes, aligning with my ultimate goal of using technology to make an impact.
         <br/><br/>
         When I'm not coding, you can find me working on my Nissan 370Z, or exploring my interests such as fishing, dirtbiking, weight lifting, chess, and spikeball.</p>
       </div>
